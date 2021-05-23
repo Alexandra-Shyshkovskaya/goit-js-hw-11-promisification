fd55e51824867d66200e3ed0a279f2279dc7697b
Alexandra-Shyshkovskaya <74681434+Alexandra-Shyshkovskaya@users.noreply.github.com>
2021-05-23 18:32:48 +0300

    done task2

---
+++ task2.js
@@ -0,0 +1,47 @@
+//Перепиши функцию toggleUserState() так, 
+//чтобы она не использовала callback - функцию callback, 
+//а принимала всего два параметра allUsers и userName и возвращала промис.
+
+// Изначальный вид функции
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName, callback) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+
+  callback(updatedUsers);
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+// Сейчас работает так
+
+toggleUserState(users, 'Mango', logger);
+toggleUserState(users, 'Lux', logger);
+
+
+//Как должно работать
+
+const toggleUserState2 = (allUsers, userName) => {
+    
+    return new Promise((resolve, reject) => {
+        const updatedUsers = allUsers.map(user =>
+            user.name === userName ? { ...user, active: !user.active } : user,
+        );
+        resolve(updatedUsers);
+    });
+  
+};
+
+const logger2 = updatedUsers => console.table(updatedUsers);
+
+//Должно работать так
+ 
+toggleUserState2(users, 'Mango').then(logger);
+toggleUserState2(users, 'Lux').then(logger);
